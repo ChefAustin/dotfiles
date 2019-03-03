d306845574f87f85b0641bd5a3a9ff15e5a02522
Austin Culter <austin.culter@gusto.com>
2019-03-02 18:26:02 -0700

    Add `Atomfile` and bundling invocation

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,8 @@ gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D69
 \curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles
 source ~/.rvm/scripts/rvm
 
-
+#### Atom packages
+/usr/local/bin/apm --packages-file ./Atomfile
 
 ##### macOS preferences
 # Decrease key-repeat times
