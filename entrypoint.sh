756b31b4aef4fad9dbd2179119f429a9df7515ba
Austin Culter <austinculter@gmail.com>
2019-04-01 19:57:05 -0600

    wha

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,8 +44,8 @@ defaults write com.apple.dock autohide -bool true
 # /usr/local/bin/gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 # Install rvm and source changes
-#\curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles
-#source ~/.rvm/scripts/rvm
+# \curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles
+# source ~/.rvm/scripts/rvm
 
 ##### App preferences
 ## iTerm2
